4821d7b9b1f44b8ae6db2af687014278292cf629
Volkov_Pavlo <javolkov.p@gmail.com>
2024-01-20 18:59:13 +0200

    file updated

--- tests/blognavigation.spec.ts
+++ tests/blognavigation.spec.ts
@@ -2,6 +2,16 @@ import { test, expect } from '@playwright/test';
 
 test('blog navigation', async ({ page }) => {
   await page.goto('https://ilarionhalushka.github.io/');
+
+  await page.getByText('Cracking Google Summer of Code').and (page.locator('a')).click();
+  await page.getByText('Back to the list of articles').click();
+
+  await page
+    .locator('.posts-list')
+    .filter({ hasText:'How I Failed At Amazon Interview Because of LP'})
+    .click();
+  await page.pause();
+
   const page1Promise = page.waitForEvent('popup');
   await page.getByRole('link', { name: 'Do Not EVER Accept The' }).first().click();
   const page1 = await page1Promise;
